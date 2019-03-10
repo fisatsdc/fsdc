aac727728c9e4ecabe88609a0213fc8bf47eb934
Tangerine98 <menonnandini98@gmail.com>
2019-03-11 01:47:49 +0530

    Fix #26: Close register menu on click anywhere

--- js/register.js
+++ js/register.js
@@ -1,10 +1,14 @@
 var body = $('body');
 var toggle = $('.register-menu');
-var list = $('.register-list')
+var list = $('.register-list');
+var click_count = 0;
+var rem = 0;
 
 toggle.on("click", function() {
   $(this).toggleClass('is-closed');
+  console.log(toggle);
   if(!$(this).hasClass('is-closed')) {
+      rem = click_count%2;
       body.addClass('is-open');
       list.removeClass('is-invisible');
   } else {
@@ -12,6 +16,21 @@ toggle.on("click", function() {
       list.addClass('is-invisible');
   }
 });
+
+window.onclick = function(event) {
+  click_count += 1;
+  console.log(click_count);
+  no = click_count%2;
+  console.log("no : ",no);
+  if(click_count%2==rem) {
+    if(body.hasClass('is-open')) {
+      body.removeClass('is-open');
+      list.addClass('is-invisible');
+      toggle.toggleClass('is-closed');
+      flag = false;
+    }
+  }
+}
 
 $('.list-text').hover(function () {
   $(this).css('color', '#e65300' );
