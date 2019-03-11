54b0a09a6b0f024598b42c4a97ec87b9f798f2a5
mohitrajane <mhit98@gmail.com>
2019-03-11 23:32:57 +0530

    Prettierfied the files

--- js/handburger-fix.js
+++ js/handburger-fix.js
@@ -1,13 +1,14 @@
-var burger = document.querySelector('.burger');
-var menu = document.querySelector('#'+burger.dataset.target);
-var  navbarItem = document.querySelectorAll('.navbar-item');
-if(navbarItem){
-    navbarItem.forEach(function(el){
-        if(!el.innerHTML.includes("REGISTER")){
-            el.addEventListener('click',function(){
-                burger.classList.toggle('is-active');
-                menu.classList.toggle('is-active');
-        });
+var burger = document.querySelector(".burger");
+var menu = document.querySelector("#" + burger.dataset.target);
+var navbarItem = document.querySelectorAll(".navbar-item");
+if (navbarItem) {
+  navbarItem.forEach(function(el) {
+    if (!el.innerHTML.includes("REGISTER")) {
+      //To check if not register and remove is-active
+      el.addEventListener("click", function() {
+        burger.classList.toggle("is-active");
+        menu.classList.toggle("is-active");
+      });
     }
-    });
+  });
 }
