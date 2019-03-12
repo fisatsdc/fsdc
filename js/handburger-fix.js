625448dae22a53cb975a135610d399f9a09da558
mohitrajane <mhit98@gmail.com>
2019-03-12 09:39:27 +0530

    fix #41 add register close also

--- js/handburger-fix.js
+++ js/handburger-fix.js
@@ -1,3 +1,4 @@
+var body = $("body");
 var burger = document.querySelector(".burger");
 var menu = document.querySelector("#" + burger.dataset.target);
 var navbarItem = document.querySelectorAll(".navbar-item");
@@ -9,6 +10,21 @@ if (navbarItem) {
         burger.classList.toggle("is-active");
         menu.classList.toggle("is-active");
       });
+    } else {
+      var registerList = document.querySelector(".register-list");
+      var registerMenu = document.querySelector(".register-menu");
+      var registerItems = document.querySelectorAll(".list-text");
+      if (registerItems) {
+        registerItems.forEach(function(item) {
+          item.addEventListener("click", function() {
+            registerMenu.classList.toggle("is-closed");
+            registerList.classList.toggle("is-invisible");
+            burger.classList.toggle("is-active");
+            menu.classList.toggle("is-active");
+            body.removeClass("is-open");
+          });
+        });
+      }
     }
   });
 }
