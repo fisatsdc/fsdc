4fc183b30789a910666207480936776831ace070
Tangerine98 <menonnandini98@gmail.com>
2019-03-14 23:34:24 +0530

    Fix hamburger-close issue

--- js/register.js
+++ js/register.js
@@ -20,50 +20,30 @@ toggle.unbind('click').on("click", function() {
 
 window.onclick = function(event) {
   if (navbarMenu.hasClass('is-active')) {
-    //click_count += 1;
-    //console.log("click_count : ",click_count);
-    //if (click_count%2 == 0) {
-      if(body.hasClass('is-open')) {
-        register_click += 1;
-        console.log('register-click : ',register_click);
-        if (register_click%2 == 0) {
-          body.removeClass('is-open');
-          list.addClass('is-invisible');
-          toggle.toggleClass('is-closed');
-          //click_count = 1;
-        }
-      } else {
-        hamburger_click += 1;
-        if (hamburger_click%2 == 0) {
+    if(body.hasClass('is-open')) {
+      register_click += 1;
+      console.log('register-click : ',register_click);
+      if (register_click%2 == 0) {
+        body.removeClass('is-open');
+        list.addClass('is-invisible');
+        toggle.toggleClass('is-closed');
+      }
+    } else {
+      hamburger_click += 1;
+      if (hamburger_click%2 == 0) {
         burger.classList.toggle("is-active");
         menu.classList.toggle("is-active");
-        //click_count = 0;
       }
-      }
-    //} 
+    }
   } else {
     click_count += 1;
     if (click_count%2 == 0) {
       body.removeClass('is-open');
-          list.addClass('is-invisible');
-          toggle.toggleClass('is-closed');
+      list.addClass('is-invisible');
+      toggle.toggleClass('is-closed');
     }
   }
 }
-
-/*
-//To close hamburger-menu when clicked anywhere
-window.onclick = function(event) {
-  if(navbarMenu.hasClass('is-active')) {
-    click_count = click_count +1;
-    if (click_count%2 == 0) {
-      console.log('enter if');
-      burger.classList.toggle("is-active");
-      menu.classList.toggle("is-active");
-    }
-  }
-}
-*/
 
 $('.list-text').hover(function () {
   $(this).css('color', '#e65300' );
